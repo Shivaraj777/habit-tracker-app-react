96e2e1b6a9cf891ca71a9f3f899f2593f5559405
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-07-29 13:02:59 +0530

    initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <div className='nav'>
+      <div className='nav-left'>
+        <span>Habit Tracker</span>
+      </div>
+      <div className='nav-right'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
